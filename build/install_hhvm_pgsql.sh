22c21d533180959601cc407f9d810a9f88e94710
Austin Hyde <austin109@gmail.com>
2015-06-30 00:46:31 -0400

    Maybe we can install hhvm-dev without sudo?

--- build/install_hhvm_pgsql.sh
+++ build/install_hhvm_pgsql.sh
@@ -7,6 +7,14 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$DIR"
 
 if [[ "$(php --version)" == HipHop* ]] ; then
+  # install deps
+  apt-get install python-software-properties
+  add-apt-repository ppa:mapnik/boost
+  apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449
+  echo deb http://dl.hhvm.com/ubuntu precise main | sudo tee /etc/apt/sources.list.d/hhvm.list
+  apt-get update
+  apt-get install hhvm
+
   # clone it
   git clone https://github.com/PocketRent/hhvm-pgsql.git
   cd hhvm-pgsql
