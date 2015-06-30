e1eca4d15435b751af4115d6fd9cddc8bb1e130b
Austin Hyde <austin109@gmail.com>
2015-06-30 02:10:40 -0400

    Install ALL the things!

--- build/install_hhvm_pgsql.sh
+++ build/install_hhvm_pgsql.sh
@@ -1,19 +1,37 @@
 #!/usr/bin/env bash
 
+# exit on first error
 set -e
 
+# get the directory this script is in (build/) and cd to it
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
 cd "$DIR"
 
 if [[ "$(php --version)" == HipHop* ]] ; then
-  # install deps
+  # install deps: gcc-4.8, hhvm-dev, libpq, boost, jemalloc, glog
   sudo apt-get install python-software-properties
   sudo add-apt-repository -y ppa:mapnik/boost
-  sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449
+  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+  sudo add-apt-repository -y ppa:boost-latest/ppa
+  sudo apt-add-repository 'deb http://ftp.osuosl.org/pub/mariadb/repo/5.5/ubuntu precise main'
   echo deb http://dl.hhvm.com/ubuntu precise main | sudo tee /etc/apt/sources.list.d/hhvm.list
+  
+  sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CBCB082A1BB943DB
+
   sudo apt-get update
-  sudo apt-get install hhvm-dev
+  sudo apt-get install gcc-4.8 g++-4.8 hhvm-dev libpq-dev libboost1.55-all-dev libjemalloc-dev
+  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
+  sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
+
+  # glog needs installed from source
+  wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
+  tar zxvf glog-0.3.3.tar.gz
+  cd glog-0.3.3
+  ./configure
+  make
+  sudo make install
+  cd $DIR
 
   # clone it
   git clone https://github.com/PocketRent/hhvm-pgsql.git
